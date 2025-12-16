6fc5a715bd38cd6a67b4fd07959c182233a2e21d
zou-hong-run <65335712+zou-hong-run@users.noreply.github.com>
2025-12-16 15:34:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import https from 'https'
 import { Server } from 'socket.io'
 import { getIpAddress } from "./utils/common.js";
 // 一对一通讯的时候使用这个
-// import { initSDPServer } from "./server/sdp.js";
+import { initSDPServer } from "./server/sdp.js";
 // 多对多通讯的时候使用这个
-import { initSDPServer } from "./server/sdp2.js";
+// import { initSDPServer } from "./server/sdp2.js";
 const options = {
   key: fs.readFileSync(path.resolve("./ssl/server.key")),
   cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
